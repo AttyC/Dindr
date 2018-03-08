c31f39ae22986f9cf95390ef2d660c0c53c16c93
Simon Bao <Simonbao.sb@gmail.com>
2018-03-08 22:47:19 +0000

    server implements api

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 import config from './config';
 import express from 'express';
+import apiRouter from './api';
 const server = express();
+server.use('/api', apiRouter);
 
 server.get('/', (req, res)=>{
     res.send('hello');
 })
 
-server.listen(config.port);
+server.listen(config.port, ()=>{
+    console.log('Server listening on:', config.port);
+});
