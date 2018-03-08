13e293a55a3729bcff2bcd4991317a4ab04aeaef
Simon Bao <Simonbao.sb@gmail.com>
2018-03-08 23:02:58 +0000

    Implements and uses new 'ejs' files and implemented backup public html files

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ import express from 'express';
 import apiRouter from './api';
 const server = express();
 server.use('/api', apiRouter);
+server.set('view engine', 'ejs');
+server.use(express.static('public'));
 
 server.get('/', (req, res)=>{
-    res.send('hello');
+    res.render('index');
 })
-
 server.listen(config.port, ()=>{
     console.log('Server listening on:', config.port);
 });
