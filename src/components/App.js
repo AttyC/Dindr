fdd2d32a21be9e309a59c59ffa484d6b663a36cc
Simon Bao <Simonbao.sb@gmail.com>
2018-03-09 10:24:21 +0000

    Using partials

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return(
+        <div className='App'>
+            <h2>React Component!!</h2>
+        </div>
+    )
+}
+
+export default App;
