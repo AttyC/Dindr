874cab03435a4da4231be26977e052c65a60a4ec
Simon Bao <Simonbao.sb@gmail.com>
2018-03-12 16:38:20 +0000

    Implemented Routing

---
+++ src/components/Users.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Users extends React.Component {
+  render() {
+    return (
+       <div className="Users">
+       <h1>Developers</h1>
+       </div>
+    );
+  }
+}
+
+export default Users;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import Users from './components/Users';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const BasicExample = () => (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+  
+        <hr />
+  
+        <Route exact path="/" component={App} />
+        <Route exact path="/users" component={Users} />
+      </div>
+    </Router>
+  );
+  
 
 ReactDOM.render(
-    <App />,
+      <BasicExample />,
     document.getElementById('root')
 );
