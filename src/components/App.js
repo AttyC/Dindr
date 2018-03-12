de3d0c25b82b59d12cd47c0a441686e02fc6aab7
Simon Bao <Simonbao.sb@gmail.com>
2018-03-12 18:03:41 +0000

    Refactor users.

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,17 @@
 import React from 'react';
 import axios from 'axios';
 import UserNewForm from './UserNewForm';
-import UserSearchSkillsForm from './UserSearchSkillsForm';
-import UserList from './UserList';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { users: [] };
-    this.loadUsersFromServer = this.loadUsersFromServer.bind(this);
-  }
-
-  loadUsersFromServer(){
-    axios.get('/api/users')
-    .then(res => {
-      this.setState({ users: res.data });
-    });
-  }
-
-  handleSearchSkills(res){
-    this.setState({ users: res.data });
-  }
-
-  componentDidMount(){
-    this.loadUsersFromServer();
-    // setInterval(this.loadUsersFromServer, this.props.pollInterval);
   }
 
   render() {
     return (
        <div className="App">
        <h1>Users</h1>
-       < UserSearchSkillsForm searchSkills={this.handleSearchSkills.bind(this)}/>
        <h3>Add new User:</h3>< UserNewForm />
-       <UserList users={ this.state.users} />
        </div>
     );
   }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,36 @@
 import React from 'react';
-
+import UserList from './UserList';
+import UserSearchSkillsForm from './UserSearchSkillsForm';
+import axios from 'axios';
 
 class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {users: []};
+    this.loadUsersFromServer = this.loadUsersFromServer.bind(this);
+  }
+
+  loadUsersFromServer(){
+    axios.get('/api/users')
+    .then(res => {
+      this.setState({ users: res.data });
+    });
+  }
+
+  handleSearchSkills(res){
+    this.setState({ users: res.data });
+  }
+
+  componentDidMount(){
+    this.loadUsersFromServer();
+  }
+
   render() {
     return (
        <div className="Users">
        <h1>Developers</h1>
+       < UserSearchSkillsForm searchSkills={this.handleSearchSkills.bind(this)}/>
+       <UserList users={this.state.users} />
        </div>
     );
   }
